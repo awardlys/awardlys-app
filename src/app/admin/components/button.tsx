e37f7f0b5908f8f603bb51809753e9a82227e8ab
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-12-23 01:11:11 -0300

    feat(components): set current page and navigate to clicked

--- src/app/admin/components/button.tsx
+++ src/app/admin/components/button.tsx
@@ -1,11 +1,30 @@
-type Props = {
-  icon?: React.ReactNode;
-  children: React.ReactNode;
-};
+import { ComponentProps, ReactNode } from "react";
+import { VariantProps, tv } from "tailwind-variants";
 
-export const Button = ({ children, icon }: Props) => {
+const button = tv({
+  base: [
+    "flex gap-2 items-center rounded px-2 py-1 transition ease-in-out delay-150 w-full ",
+  ],
+  variants: {
+    variant: {
+      primary: "bg-gray-600 text-zinc-300 hover:bg-gray-700",
+      current: "bg-purple-600 text-purple-300",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+type ButtonProps = {
+  icon?: ReactNode;
+  variant?: string;
+} & ComponentProps<"button"> &
+  VariantProps<typeof button>;
+
+export const Button = ({ children, icon, variant, ...props }: ButtonProps) => {
   return (
-    <button className="flex gap-2 items-center rounded px-2 py-1 transition ease-in-out delay-150 w-full hover:bg-violet-600 hover:text-violet-200">
+    <button {...props} className={button({ variant })}>
       {icon}
       {children}
     </button>
--- src/app/admin/components/sidebar.tsx
+++ src/app/admin/components/sidebar.tsx
@@ -1,12 +1,20 @@
+"use client";
 import {
   ArchiveBoxIcon,
   ArrowRightStartOnRectangleIcon,
   TagIcon,
   TrophyIcon,
 } from "@heroicons/react/20/solid";
+import { usePathname, useRouter } from "next/navigation";
 import { Button } from "./button";
 
 export function Sidebar() {
+  const { push } = useRouter();
+  const path = usePathname();
+  const page = path.split("/")[2];
+  const current = (section: string) =>
+    page === section ? "current" : "primary";
+
   return (
     <aside className="flex flex-col justify-between h-full pb-2">
       <div>
@@ -14,15 +22,29 @@ export function Sidebar() {
         <nav>
           <ul className="flex flex-col gap-2">
             <li>
-              <Button icon={<TrophyIcon className="h-4 w-4" />}>
+              <Button
+                onClick={() => push("/admin/awards")}
+                variant={current("awards")}
+                icon={<TrophyIcon className="h-4 w-4" />}
+              >
                 Premiações
               </Button>
             </li>
             <li>
-              <Button icon={<TagIcon className="h-4 w-4" />}>Categorias</Button>
+              <Button
+                onClick={() => push("/admin/categories")}
+                variant={current("categories")}
+                icon={<TagIcon className="h-4 w-4" />}
+              >
+                Categorias
+              </Button>
             </li>
             <li>
-              <Button icon={<ArchiveBoxIcon className="h-4 w-4" />}>
+              <Button
+                onClick={() => push("/admin/items")}
+                variant={current("items")}
+                icon={<ArchiveBoxIcon className="h-4 w-4" />}
+              >
                 Itens
               </Button>
             </li>
