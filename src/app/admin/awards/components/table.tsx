194ca4bb6fae31a79bbb77d5f6e8038377f5a726
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-12-24 13:21:21 -0300

    feat(categories): create table and search

--- src/app/admin/awards/components/table.tsx
+++ src/app/admin/awards/components/table.tsx
@@ -1,33 +1,42 @@
 import { awards } from "@/data/awards";
-import { table } from "console";
-
 
 export function Table() {
-    return (
-        <table className="w-full border border-gray-600 rounded-md overflow-hidden">
-            <thead>
-                <tr className="text-left border-b border-gray-600 bg-slate-800">
-                    <th className="p-3">Titulo</th>
-                    <th className="p-3">Subtitulo</th>
-                    <th className="p-3">Criação</th>
-                    <th className="p-3">Atualização</th>
-                    <th >Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {awards.map(item => (
-                    <tr key={item.id} className="text-gray-800 bg-gray-400 border-b border-gray-600">
-                        <td className="flex p-3 items-center">{item.title}</td>
-                        <td>{item.subTitle}</td>
-                        <td>{item.createdAt}</td>
-                        <td>{item.updatedAt}</td>
-                        <td>
-                            {item.status && <div className="px-2 py-1 inline-block rounded-md border-green-800 bg-green-600 text-white text-xs">Ativo</div>}
-                            {!item.status && <div className="px-2 py-1 inline-block rounded-md border-red-800 bg-red-600 text-white text-xs">Expirado</div>}
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    )
-} 
+  return (
+    <table className="w-full border border-gray-600 rounded-md overflow-hidden">
+      <thead>
+        <tr className="text-left text-gray-300 border-b border-gray-600 bg-slate-800">
+          <th className="p-3">Titulo</th>
+          <th className="p-3">Subtitulo</th>
+          <th className="p-3">Criação</th>
+          <th className="p-3">Atualização</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {awards.map((item) => (
+          <tr
+            key={item.id}
+            className="text-gray-800 bg-gray-400 border-b border-gray-600"
+          >
+            <td className="flex p-3 items-center">{item.title}</td>
+            <td>{item.subTitle}</td>
+            <td>{item.createdAt}</td>
+            <td>{item.updatedAt}</td>
+            <td>
+              {item.status && (
+                <div className="px-2 py-1 inline-block rounded-md border-green-800 bg-green-600 text-white text-xs">
+                  Ativo
+                </div>
+              )}
+              {!item.status && (
+                <div className="px-2 py-1 inline-block rounded-md border-red-800 bg-red-600 text-white text-xs">
+                  Expirado
+                </div>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,3 +1,36 @@
+"use client";
+import { awards } from "@/data/awards";
+import { Table } from "antd";
+import Search, { SearchProps } from "antd/es/input/Search";
+import { useState } from "react";
+
 export default function Categories() {
-  return <div>Categories</div>;
+  const [search, setSearch] = useState(awards);
+  const onSearch: SearchProps["onSearch"] = (value) => {
+    console.log(value);
+    const search = awards.filter((item) =>
+      item.title.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+    );
+    setSearch(search);
+  };
+
+  return (
+    <div className=" flex flex-col p-3 w-full gap-2">
+      <Search className="w-full" placeholder="Pesquise" onSearch={onSearch} />
+      <Table
+        dataSource={search}
+        columns={[
+          {
+            dataIndex: "title",
+            title: "Nome",
+            sorter: (a, b) => a.title.localeCompare(b.title),
+          },
+          {
+            dataIndex: "subTitle",
+            title: "Descrição",
+          },
+        ]}
+      />
+    </div>
+  );
 }
